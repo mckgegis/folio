113fdea9e8d4ec4d2d9d9140a5a66e45fd098b9d
mckgegis <mckgegis@yahoo.com>
2021-01-27 20:55:59 -0800

    Updates

---
+++ precache-manifest.0a272957006d13766ed0aa23deafab43.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe9b30ca40b2d047fc975fa19ba714bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8984422d59fab13d6bd2",
+    "url": "/static/css/2.eb43dee1.chunk.css"
+  },
+  {
+    "revision": "efed92dbbd84492fdf24",
+    "url": "/static/css/main.5a9c62d9.chunk.css"
+  },
+  {
+    "revision": "8984422d59fab13d6bd2",
+    "url": "/static/js/2.b7d64a41.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.b7d64a41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efed92dbbd84492fdf24",
+    "url": "/static/js/main.816ff505.chunk.js"
+  },
+  {
+    "revision": "ae7bb63ef3bb4426842b",
+    "url": "/static/js/runtime-main.6f3f843c.js"
+  },
+  {
+    "revision": "0f964a72f8fbdf9c8d4e0122b8effb40",
+    "url": "/static/media/devicon.0f964a72.woff"
+  },
+  {
+    "revision": "29f80b00a7b4641183f96f08be374697",
+    "url": "/static/media/devicon.29f80b00.ttf"
+  },
+  {
+    "revision": "5a844b1606f41f40b52041c1fd279502",
+    "url": "/static/media/hawaii.5a844b16.svg"
+  },
+  {
+    "revision": "6c9efe0c8c1fb630cd48d0381ece4297",
+    "url": "/static/media/left-arrow.6c9efe0c.svg"
+  },
+  {
+    "revision": "3f09b2eb29ec3949629497642e1fe23b",
+    "url": "/static/media/pepperdine.3f09b2eb.svg"
+  },
+  {
+    "revision": "be237328d9c252b1b9133a02c3be3e5e",
+    "url": "/static/media/right-arrow.be237328.svg"
+  },
+  {
+    "revision": "f37ba7ddf7ff25274b716f968c9f21a7",
+    "url": "/static/media/sheep.f37ba7dd.gif"
+  },
+  {
+    "revision": "5eb72d9df0841bda6cce61dd78cff793",
+    "url": "/static/media/sheepapparel.5eb72d9d.png"
+  },
+  {
+    "revision": "8ea96302f16eda8d1e14d279a7c9ece4",
+    "url": "/static/media/sheepcart.8ea96302.png"
+  },
+  {
+    "revision": "b662fc3cf3fa5e3bed418b8919a042f5",
+    "url": "/static/media/sheepsneakers.b662fc3c.png"
+  },
+  {
+    "revision": "c104b1eb469a9d2958247c7f194fff93",
+    "url": "/static/media/sheepsplash.c104b1eb.png"
+  },
+  {
+    "revision": "062878e87f0718943c571906939f928d",
+    "url": "/static/media/theradogs.062878e8.png"
+  },
+  {
+    "revision": "95db974cdb52c2bd0ecf72649d84d24e",
+    "url": "/static/media/theraprofile.95db974c.png"
+  },
+  {
+    "revision": "5faa0e2369e7b413c40a45e0fbba6ec2",
+    "url": "/static/media/therapuppy.5faa0e23.png"
+  },
+  {
+    "revision": "eb3a77e3844994775aff03f125b42965",
+    "url": "/static/media/therapuppy.eb3a77e3.gif"
+  },
+  {
+    "revision": "095ec29a0c6c8453fe4b4d52f646941d",
+    "url": "/static/media/trello-board.095ec29a.png"
+  },
+  {
+    "revision": "89627b2505269ee918497456efca0c5b",
+    "url": "/static/media/trello-main.89627b25.png"
+  },
+  {
+    "revision": "283ae9d9a16f7a042fcc4f5df826c73b",
+    "url": "/static/media/trello.283ae9d9.gif"
+  },
+  {
+    "revision": "e1a5ad160f18f7a1d94a0b61e9b24b01",
+    "url": "/static/media/trelloSplash.e1a5ad16.png"
+  }
+]);
