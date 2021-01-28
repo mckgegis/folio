57b4cc263e7699124a59016f8bbd69ffcd46d62f
mckgegis <mckgegis@yahoo.com>
2021-01-27 21:33:52 -0800

    fix splash background

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import './App.scss';
 import React from 'react';
-import { Route } from 'react-router-dom';
-import Splash from './Components/Splash';
-import Projects from './Components/Projects'
 import Test from './Components/Test';
 
 
 function App() {
   return (
     <div className="App">
-      <Route exact path="/" component={Test} />
-      <Route path="/projects" component={Projects} />
+      <Test />
     </div>
   );
 }
